6b2c9815b331543407e7bbda46bf161066a13c80
Dave ZAGABE <80624123+DaveZag@users.noreply.github.com>
2022-12-22 15:56:46 +0200

    Implement my custom enumerable

---
+++ enum_module.rb
@@ -0,0 +1,24 @@
+module MyEnumerable
+    def all?
+      each do |i|
+        return true unless block_given?
+  
+        return false unless yield(i)
+      end
+      true
+    end
+  
+    def any?
+      each do |i|
+        return true unless block_given?
+        return true if yield(i)
+      end
+      false
+    end
+  
+    def enum_filter?
+      array = []
+      each { |i| array.push(i) if yield(i) }
+      array
+    end
+  end
